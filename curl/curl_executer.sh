2b2a4ac1f5a08566167408429985ffaac8fe1685
dni <daniele@nicastro.io>
2023-01-07 00:39:43 +0100

    curl: move file

--- curl/curl_executer.sh
+++ curl/curl_executer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 counter=0
-maxfiles=0
+maxfiles=10
 for filename_json in ../processing/url/*.json; do
   [ -e "$filename_json" ] || continue
 
@@ -12,10 +12,11 @@ for filename_json in ../processing/url/*.json; do
   #  echo "$filename_json"
   url=$(jq .url -r "$filename_json")
   filename_output="$(date +%s)_$(jq .type -r "$filename_json")_$(jq .check_id -r "$filename_json")_$(jq .check_item_id -r "$filename_json").txt"
-  echo $filename_output
+  echo $filename_json
   #  echo "$url"
   #  url="https://bing.com"
   /usr/bin/curl -w "@curl-format.txt" --request GET --compressed -Lvs -o /dev/null ${url} > ../processed/url/$filename_output 2>&1
+  mv $filename_json ../processing/url/done/
   #echo $test > a.txt 2>&1
   # ... rest of the loop body
 done
