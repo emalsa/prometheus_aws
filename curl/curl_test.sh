7dbfc46470f0e3421d194add3ec26fcfc90b6f2b
dni <daniele@nicastro.io>
2023-01-07 10:36:39 +0100

    log messages

--- curl/curl_test.sh
+++ curl/curl_test.sh
@@ -2,22 +2,26 @@
 
 counter=0
 maxfiles=10
-echo "Hello"
+echo "Hello 1"
 for filename_json in ../processing/url/*.json; do
   [ -e "$filename_json" ] || continue
 
   if [[ $counter -gt $maxfiles ]]; then
     break
   fi
+  echo "Hello 2"
   counter=$(($counter + 1))
   #  echo "$filename_json"
   url=$(jq .url -r "$filename_json")
   filename_output="TEST_$(date +%s)_$(jq .type -r "$filename_json")_$(jq .check_id -r "$filename_json")_$(jq .check_item_id -r "$filename_json").txt"
-#  echo $filename_json
+  echo "Hello 3"
+  #  echo $filename_json
   #  echo "$url"
   #  url="https://bing.com"
   /usr/bin/curl --request GET --compressed -Lvs -o /dev/null https://prometheus.nicastro.io/cron_test >../processed/url/$filename_output 2>&1
+  echo "Hello 4"
   #mv $filename_json ../processing/url/done/
   #echo $test > a.txt 2>&1
   # ... rest of the loop body
 done
+echo "Hello 5"
