6cba13fd3f93398e8b67f0bde1036e8d415e6507
dni <daniele@nicastro.io>
2023-01-07 14:51:00 +0100

    format file

--- curl/curl_executer.sh
+++ curl/curl_executer.sh
@@ -11,6 +11,6 @@ for filename_json in /var/www/html/processing/url/*.json; do
   counter=$(($counter + 1))
   url=$(jq .url -r "$filename_json")
   filename_output="$(date +%s)_$(jq .type -r "$filename_json")_$(jq .check_id -r "$filename_json")_$(jq .check_item_id -r "$filename_json").txt"
-  /usr/bin/curl -w "/var/www/html/@curl-format.txt" --request GET --compressed -Lvs -o /dev/null ${url} > /var/www/html/processed/url/$filename_output 2>&1
+  /usr/bin/curl -w "@/var/www/html/curl-format.txt" --request GET --compressed -Lvs -o /dev/null ${url} > /var/www/html/processed/url/$filename_output 2>&1
   mv $filename_json /var/www/html/processing/url/done/
 done
